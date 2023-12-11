48cc40fff302879b89c4589ad141576d2ea46607
relchman <relchmanaugusto@gmail.com>
2023-12-11 09:17:04 -0300

    feat(projeto):hammer::Primeiro commit

---
+++ src/main/java/com/estudos/microservice/eclienteapi/EClienteApiApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.microservice.eclienteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EClienteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EClienteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudos/microservice/eclienteapi/EClienteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.microservice.eclienteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EClienteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
